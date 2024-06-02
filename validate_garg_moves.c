b38394251d474ff4ce679d611b79b4dfb63402cc
neostreet <lloyd.aidan@gmail.com>
2024-06-02 13:03:56 -0400

    add validate_garg_moves.c

---
+++ validate_garg_moves.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "garg.h"
+#define MAKE_GLOBALS_HERE
+#include "garg.glb"
+#include "garg.fun"
+#include "garg.mac"
+
+#define MAX_FILENAME_LEN 256
+static char filename[MAX_FILENAME_LEN];
+
+static char usage[] =
+"usage: validate_garg_moves (-verbose) filename\n";
+
+int bHaveGame;
+int afl_dbg;
+
+char couldnt_get_status[] = "couldn't get status of %s\n";
+char couldnt_open[] = "couldn't open %s\n";
+
+int main(int argc,char **argv)
+{
+  int n;
+  int curr_arg;
+  bool bVerbose;
+  int retval;
+  FILE *fptr;
+  int filename_len;
+  struct game curr_game;
+  bool bBlack;
+  int dbg;
+
+  if ((argc < 2) || (argc > 3)) {
+    printf(usage);
+    return 1;
+  }
+
+  bVerbose = false;
+
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-verbose"))
+      bVerbose = true;
+    else
+      break;
+  }
+
+  if (argc - curr_arg != 1) {
+    printf(usage);
+    return 2;
+  }
+
+  if ((fptr = fopen(argv[curr_arg],"r")) == NULL) {
+    printf(couldnt_open,argv[curr_arg]);
+    return 3;
+  }
+
+  for ( ; ; ) {
+    GetLine(fptr,filename,&filename_len,MAX_FILENAME_LEN);
+
+    if (feof(fptr))
+      break;
+
+    bzero(&curr_game,sizeof (struct game));
+
+    retval = read_binary_game(filename,&curr_game);
+
+    if (retval) {
+      printf("read_binary_game of %s failed: %d\n",filename,retval);
+      printf("curr_move = %d\n",curr_game.curr_move);
+
+      continue;
+    }
+
+    set_initial_board(&curr_game);
+
+    for (curr_game.curr_move = 0; curr_game.curr_move < curr_game.num_moves; curr_game.curr_move++) {
+      bBlack = curr_game.curr_move & 0x1;
+      update_board(&curr_game,NULL,NULL);
+
+      if (curr_game.curr_move == dbg_move)
+        dbg = 1;
+
+      if (player_is_in_check(bBlack,curr_game.board)) {
+        if (!bVerbose)
+          printf("%s\n",filename);
+        else {
+          printf("%s: on move %d, %s didn't respond to a check made on the previous move\n",
+            filename,curr_game.curr_move,(bBlack ? "Black" : "White"));
+          print_bd0(curr_game.board,curr_game.orientation);
+        }
+
+        break;
+      }
+    }
+
+  }
+
+  return 0;
+}
