43c2ad2b1041c3e215189026d874429d8fb8354d
neostreet <lloyd.aidan@gmail.com>
2024-06-19 11:47:40 -0400

    synch up with gargantua

--- gargmvs.c
+++ gargmvs.c
@@ -113,8 +113,6 @@ int do_pawn_move(struct game *gamept)
   return retval; // success
 }
 
-char piece_ids[] = "RNBQKG";
-
 int (*piece_functions[])(struct game *) = {
   rook_move2,
   knight_move2,
