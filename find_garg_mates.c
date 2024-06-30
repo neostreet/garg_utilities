a4ad1ca022c65b83ad8f40ffa80c5bf84d88b649
neostreet <lloyd.aidan@gmail.com>
2024-06-29 22:29:44 -0400

    add find_garg_mates.c

---
+++ find_garg_mates.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "garg.h"
+#define MAKE_GLOBALS_HERE
+#include "garg.glb"
+#include "garg.fun"
+#include "garg.mac"
+
+#define MAX_FILENAME_LEN 256
+static char filename[MAX_FILENAME_LEN];
+
+static char usage[] =
+"usage: find_garg_mates filename\n";
+
+int bHaveGame;
+int afl_dbg;
+
+char couldnt_get_status[] = "couldn't get status of %s\n";
+char couldnt_open[] = "couldn't open %s\n";
+
+int main(int argc,char **argv)
+{
+  int n;
+  int retval;
+  FILE *fptr;
+  int filename_len;
+  struct game curr_game;
+  unsigned char board[CHARS_IN_BOARD];
+
+  if (argc != 2) {
+    printf(usage);
+    return 1;
+  }
+
+  if ((fptr = fopen(argv[1],"r")) == NULL) {
+    printf(couldnt_open,argv[1]);
+    return 2;
+  }
+
+  for ( ; ; ) {
+    GetLine(fptr,filename,&filename_len,MAX_FILENAME_LEN);
+
+    if (feof(fptr))
+      break;
+
+    bzero(&curr_game,sizeof (struct game));
+
+    retval = read_binary_game(filename,&curr_game);
+
+    if (retval) {
+      printf("read_binary_game of %s failed: %d\n",filename,retval);
+      printf("curr_move = %d\n",curr_game.curr_move);
+
+      continue;
+    }
+
+    legal_moves_count = 0;
+    get_legal_moves(&curr_game,&legal_moves[0],&legal_moves_count);
+
+    if (!legal_moves_count) {
+      if (curr_game.moves[curr_game.num_moves-1].special_move_info & SPECIAL_MOVE_MATE)
+        printf("%s\n",filename);
+    }
+  }
+
+  return 0;
+}
