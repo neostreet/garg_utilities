5a614ac9603f5f13a0f74c4b20f16c51b27881cc
neostreet <lloyd.aidan@gmail.com>
2024-06-30 12:57:40 -0400

    set SPECIAL_MOVE_CHECK on every fix, and SPECIAL_MOVE_MATE when there are no legal moves

--- fix_broken_garg_games.c
+++ fix_broken_garg_games.c
@@ -77,14 +77,40 @@ int main(int argc,char **argv)
     for (curr_game.curr_move = 0; curr_game.curr_move < curr_game.num_moves; curr_game.curr_move++) {
       bBlack = curr_game.curr_move & 0x1;
       update_board(&curr_game,NULL,NULL);
+      update_piece_info(&curr_game);
 
       if (curr_game.curr_move == dbg_move)
         dbg = 1;
 
       if (player_is_in_check(bBlack,curr_game.board)) {
+        if (bVerbose)
+          print_bd0(curr_game.board,curr_game.orientation);
+
         // truncate the game at the point where it went south
         printf("fixing %s\n",filename);
+        curr_game.moves[curr_game.curr_move - 1].special_move_info |= SPECIAL_MOVE_CHECK;
         curr_game.num_moves = curr_game.curr_move;
+
+        // return to the previous move
+        set_initial_board(&curr_game);
+
+        for (curr_game.curr_move = 0; curr_game.curr_move < curr_game.num_moves; curr_game.curr_move++) {
+          update_board(&curr_game,NULL,NULL);
+          update_piece_info(&curr_game);
+        }
+
+        if (bVerbose)
+          print_bd0(curr_game.board,curr_game.orientation);
+
+        legal_moves_count = 0;
+        get_legal_moves(&curr_game,&legal_moves[0],&legal_moves_count);
+
+        if (bVerbose)
+          printf("legal_moves_count = %d\n",legal_moves_count);
+
+        if (!legal_moves_count) {
+          curr_game.moves[curr_game.curr_move - 1].special_move_info |= SPECIAL_MOVE_MATE;
+        }
 
         retval = write_binary_game(filename,&curr_game);
 
