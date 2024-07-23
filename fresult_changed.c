192b6f10c89c6faf56b920ad51c1a022870b0415
neostreet <lloyd.aidan@gmail.com>
2024-07-23 13:27:51 -0400

    add fresult_changed.c

---
+++ fresult_changed.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "garg.h"
+#define MAKE_GLOBALS_HERE
+#include "garg.glb"
+#include "garg.fun"
+#include "garg.mac"
+
+#define MAX_FILENAME_LEN 256
+static char filename[MAX_FILENAME_LEN];
+
+static char usage[] =
+"usage: fresult_changed (-debug) filename\n";
+
+char couldnt_get_status[] = "couldn't get status of %s\n";
+char couldnt_open[] = "couldn't open %s\n";
+
+int bHaveGame;
+int afl_dbg;
+
+int main(int argc,char **argv)
+{
+  int n;
+  int curr_arg;
+  bool bDebug;
+  int retval;
+  FILE *fptr;
+  int filename_len;
+  struct game curr_game;
+  int original_result;
+  int garg_result;
+
+  if ((argc < 2) || (argc > 3)) {
+    printf(usage);
+    return 1;
+  }
+
+  bDebug = false;
+
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-debug"))
+      bDebug = true;
+    else
+      break;
+  }
+
+  if (argc - curr_arg != 1) {
+    printf(usage);
+    return 2;
+  }
+
+  if ((fptr = fopen(argv[curr_arg],"r")) == NULL) {
+    printf(couldnt_open,argv[curr_arg]);
+    return 3;
+  }
+
+  for ( ; ; ) {
+    GetLine(fptr,filename,&filename_len,MAX_FILENAME_LEN);
+
+    if (feof(fptr))
+      break;
+
+    bzero(&curr_game,sizeof (struct game));
+
+    retval = read_binary_game(filename,&curr_game);
+
+    if (retval) {
+      printf("read_binary_game of %s failed: %d\n",filename,retval);
+      printf("curr_move = %d\n",curr_game.curr_move);
+
+      continue;
+    }
+
+    original_result = curr_game.result;
+
+    if (curr_game.moves[curr_game.num_moves-1].special_move_info & SPECIAL_MOVE_MATE) {
+      if (curr_game.num_moves % 2)
+        garg_result = WHITE_WIN;
+      else
+        garg_result = BLACK_WIN;
+
+      if (original_result != garg_result)
+        printf("%s: %d changed to %d\n",filename,original_result,garg_result);
+    }
+    else if (curr_game.moves[curr_game.num_moves-1].special_move_info & SPECIAL_MOVE_STALEMATE) {
+      garg_result = DRAW;
+
+      if (original_result != garg_result)
+        printf("%s: %d changed to %d\n",filename,original_result,garg_result);
+    }
+  }
+
+  fclose(fptr);
+
+  return 0;
+}
