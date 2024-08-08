bd9b08e1db2c02ab7248fbf79a02d70020a06b48
neostreet <lloyd.aidan@gmail.com>
2024-08-07 22:29:44 -0400

    add fprint_garg_moves.c

---
+++ fprint_garg_moves.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "garg.h"
+#define MAKE_GLOBALS_HERE
+#include "garg.glb"
+#include "garg.fun"
+#include "garg.mac"
+
+#define MAX_FILENAME_LEN 256
+static char filename[MAX_FILENAME_LEN];
+static char moves_filename[MAX_FILENAME_LEN];
+
+static char usage[] =
+"usage: fprint_moves (-debug) filename\n";
+
+char couldnt_get_status[] = "couldn't get status of %s\n";
+char couldnt_open[] = "couldn't open %s\n";
+
+int bHaveGame;
+int afl_dbg;
+
+int main(int argc,char **argv)
+{
+  int n;
+  int curr_arg;
+  bool bDebug;
+  int retval;
+  FILE *fptr;
+  int filename_len;
+  struct game curr_game;
+
+  if ((argc < 2) || (argc > 3)) {
+    printf(usage);
+    return 1;
+  }
+
+  bDebug = false;
+
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-debug"))
+      bDebug = true;
+    else
+      break;
+  }
+
+  if (argc - curr_arg != 1) {
+    printf(usage);
+    return 2;
+  }
+
+  if ((fptr = fopen(argv[curr_arg],"r")) == NULL) {
+    printf(couldnt_open,argv[curr_arg]);
+    return 3;
+  }
+
+  for ( ; ; ) {
+    GetLine(fptr,filename,&filename_len,MAX_FILENAME_LEN);
+
+    if (feof(fptr))
+      break;
+
+    bzero(&curr_game,sizeof (struct game));
+
+    retval = read_binary_game(filename,&curr_game);
+
+    if (retval) {
+      printf("read_binary_game of %s failed: %d\n",filename,retval);
+      printf("curr_move = %d\n",curr_game.curr_move);
+
+      continue;
+    }
+
+    sprintf(moves_filename,"%s.moves",filename);
+    fprint_moves(curr_game.moves,curr_game.num_moves,moves_filename);
+  }
+
+  fclose(fptr);
+
+  return 0;
+}
