962b9e4fcd74363e33bd82da51d2d970a9e6e9cf
neostreet <lloyd.aidan@gmail.com>
2024-08-22 22:11:17 -0400

    add -mine and -not_mine flags

--- find_garg_mates.c
+++ find_garg_mates.c
@@ -11,7 +11,7 @@
 static char filename[MAX_FILENAME_LEN];
 
 static char usage[] =
-"usage: find_garg_mates filename\n";
+"usage: find_garg_mates (-mine) (-not_mine) filename\n";
 
 int bHaveGame;
 int afl_dbg;
@@ -22,20 +22,45 @@ char couldnt_open[] = "couldn't open %s\n";
 int main(int argc,char **argv)
 {
   int n;
+  int curr_arg;
+  bool bMine;
+  bool bNotMine;
   int retval;
   FILE *fptr;
   int filename_len;
   struct game curr_game;
   unsigned char board[CHARS_IN_BOARD];
 
-  if (argc != 2) {
+  if ((argc < 2) || (argc > 4)) {
     printf(usage);
     return 1;
   }
 
-  if ((fptr = fopen(argv[1],"r")) == NULL) {
-    printf(couldnt_open,argv[1]);
+  bMine = false;
+  bNotMine = false;
+
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-mine"))
+      bMine = true;
+    else if (!strcmp(argv[curr_arg],"-not_mine"))
+      bNotMine = true;
+    else
+      break;
+  }
+
+  if (argc - curr_arg != 1) {
+    printf(usage);
     return 2;
+  }
+
+  if (bMine && bNotMine) {
+    printf("can't specify both -mine and -not_mine\n");
+    return 3;
+  }
+
+  if ((fptr = fopen(argv[curr_arg],"r")) == NULL) {
+    printf(couldnt_open,argv[curr_arg]);
+    return 4;
   }
 
   for ( ; ; ) {
@@ -55,8 +80,35 @@ int main(int argc,char **argv)
       continue;
     }
 
-    if (curr_game.moves[curr_game.num_moves-1].special_move_info & SPECIAL_MOVE_MATE)
-      printf("%s\n",filename);
+    if (curr_game.moves[curr_game.num_moves-1].special_move_info & SPECIAL_MOVE_MATE) {
+      if (!bMine && !bNotMine) {
+        printf("%s\n",filename);
+      }
+      else if (bMine) {
+        if (curr_game.num_moves % 2) {
+          if (!curr_game.orientation) {
+            printf("%s\n",filename);
+          }
+        }
+        else {
+          if (curr_game.orientation) {
+            printf("%s\n",filename);
+          }
+        }
+      }
+      else {
+        if (curr_game.num_moves % 2) {
+          if (curr_game.orientation) {
+            printf("%s\n",filename);
+          }
+        }
+        else {
+          if (!curr_game.orientation) {
+            printf("%s\n",filename);
+          }
+        }
+      }
+    }
   }
 
   return 0;
