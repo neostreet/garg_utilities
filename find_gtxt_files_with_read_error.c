dc4131effbeac7251e9958d60f1445369a6debe4
neostreet <lloyd.aidan@gmail.com>
2025-01-21 19:23:38 -0500

    add find_gtxt_files_with_read_error.c

---
+++ find_gtxt_files_with_read_error.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "garg.h"
+#define MAKE_GLOBALS_HERE
+#include "garg.glb"
+#include "garg.fun"
+#include "garg.mac"
+
+#define MAX_FILENAME_LEN 256
+static char filename[MAX_FILENAME_LEN];
+
+static char usage[] =
+"usage: find_gtxt_files_with_read_error filename\n";
+
+int bHaveGame;
+int afl_dbg;
+
+char couldnt_get_status[] = "couldn't get status of %s\n";
+char couldnt_open[] = "couldn't open %s\n";
+
+int main(int argc,char **argv)
+{
+  int n;
+  FILE *fptr;
+  int filename_len;
+  int retval;
+  struct game curr_game;
+
+  if (argc != 2) {
+    printf(usage);
+    return 1;
+  }
+
+  if ((fptr = fopen(argv[1],"r")) == NULL) {
+    printf(couldnt_open,argv[1]);
+    return 2;
+  }
+
+  for ( ; ; ) {
+    GetLine(fptr,filename,&filename_len,MAX_FILENAME_LEN);
+
+    if (feof(fptr))
+      break;
+
+    bzero(&curr_game,sizeof (struct game));
+
+    retval = read_game(filename,&curr_game,err_msg);
+
+    if (retval)
+      printf("%s\n",filename);
+  }
+
+  fclose(fptr);
+
+  return 0;
+}
