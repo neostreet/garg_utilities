6a7feafe4222894990e8bdfc8af5789af6dce457
neostreet <lloyd.aidan@gmail.com>
2025-01-22 14:50:59 -0500

    add -not flag

--- find_gtxt_files_with_read_error.c
+++ find_gtxt_files_with_read_error.c
@@ -11,7 +11,7 @@
 static char filename[MAX_FILENAME_LEN];
 
 static char usage[] =
-"usage: find_gtxt_files_with_read_error filename\n";
+"usage: find_gtxt_files_with_read_error (-not) filename\n";
 
 int bHaveGame;
 int afl_dbg;
@@ -22,19 +22,35 @@ char couldnt_open[] = "couldn't open %s\n";
 int main(int argc,char **argv)
 {
   int n;
+  int curr_arg;
+  bool bNot;
   FILE *fptr;
   int filename_len;
   int retval;
   struct game curr_game;
 
-  if (argc != 2) {
+  if ((argc < 2) || (argc > 3)) {
     printf(usage);
     return 1;
   }
 
-  if ((fptr = fopen(argv[1],"r")) == NULL) {
-    printf(couldnt_open,argv[1]);
+  bNot = false;
+
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-not"))
+      bNot = true;
+    else
+      break;
+  }
+
+  if (argc - curr_arg != 1) {
+    printf(usage);
     return 2;
+  }
+
+  if ((fptr = fopen(argv[curr_arg],"r")) == NULL) {
+    printf(couldnt_open,argv[curr_arg]);
+    return 3;
   }
 
   for ( ; ; ) {
@@ -47,8 +63,14 @@ int main(int argc,char **argv)
 
     retval = read_game(filename,&curr_game,err_msg);
 
-    if (retval)
-      printf("%s\n",filename);
+    if (!bNot) {
+      if (retval)
+        printf("%s\n",filename);
+    }
+    else {
+      if (!retval)
+        printf("%s\n",filename);
+    }
   }
 
   fclose(fptr);
